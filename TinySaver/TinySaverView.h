8111ecf92f7e7c6a0d84c07c8f1ebb65bb26d65d
Tiny <tinysilk@hotmail.com>
2016-11-05 13:09:40 +0800

    Initial Commit

---
+++ TinySaver/TinySaverView.h
@@ -0,0 +1,13 @@
+//
+//  TinySaverView.h
+//  TinySaver
+//
+//  Created by Tiny on 2016/11/5.
+//  Copyright © 2016年 Tiny. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface TinySaverView : ScreenSaverView
+
+@end
---
+++ TinySaver/TinySaverView.m
@@ -0,0 +1,52 @@
+//
+//  TinySaverView.m
+//  TinySaver
+//
+//  Created by Tiny on 2016/11/5.
+//  Copyright © 2016年 Tiny. All rights reserved.
+//
+
+#import "TinySaverView.h"
+
+@implementation TinySaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
